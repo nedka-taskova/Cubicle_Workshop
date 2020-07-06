29f350546f64bb72b6c0d0f8247a7fe7844471f4
Nedka Taskova <NTaskova@intracol.com>
2020-07-06 14:32:43 +0300

    migrate to MongoDb, create models

--- models/accessory.js
+++ models/accessory.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose')
+const { saveCube } = require('../controllers/database')
 
-const CubeSchema = new mongoose.Schema({
+const AccessorySchema = new mongoose.Schema({
     name: {
         type: String,
         required: true
@@ -14,16 +15,10 @@ const CubeSchema = new mongoose.Schema({
         type: String,
         required: true
     },
-    difficulty: {
-        type: Number,
-        required: true,
-        min: 1,
-        max: 10
-    },
-    accessories: {
+    cubes: {
         type: 'ObjectId',
-        ref: 'Accessory'
+        ref: 'Cube'
     }
 })
 
-module.exports = mongoose.model('Cube', CubeSchema)
+module.exports = mongoose.model('Accessory', AccessorySchema)
