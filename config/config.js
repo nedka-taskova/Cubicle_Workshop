d85a107590eee622c5b949bac2090d4d1506da97
Nedka Taskova <NTaskova@intracol.com>
2020-07-06 12:46:59 +0300

    connect with database

--- config/config.js
+++ config/config.js
@@ -1,6 +1,10 @@
+const database = require("../controllers/database");
+
 module.exports = {
     development: {
-        port: process.env.PORT || 3000
+        port: process.env.PORT || 3000,
+        // databaseUrl: 'mongodb+srv://user:softuni-password@softuniworkshop.ixfre.mongodb.net/cubicle'
+        databaseUrl: `mongodb+srv://user:${process.env.DB_PASSWORD}@softuniworkshop.ixfre.mongodb.net/cubicle`
     },
     production: {}
 };
--- index.js
+++ index.js
@@ -1,9 +1,23 @@
 const env = process.env.NODE_ENV || 'development';
 
+const mongoose = require('mongoose');
 const config = require('./config/config')[env];
 const express = require('express');
 const indexRouter = require('./routes')
 const app = express();
+
+mongoose.connect(config.databaseUrl, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+},(error) => {
+    if(error){
+        console.log(error)
+        throw error
+    }
+
+    console.log('Database is setup and running.');
+});
+
 
 require('./config/express')(app);
 
