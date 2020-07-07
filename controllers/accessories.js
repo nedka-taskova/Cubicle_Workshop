ad6528b7970cf904b479719468b44920ad35fd07
Nedka Taskova <Ntaskova@intracol.com>
2020-07-07 17:16:12 +0300

    new accessories page is added

---
+++ controllers/accessories.js
@@ -0,0 +1,22 @@
+const Accessory = require('../models/accessory')
+
+const getAccessories = async() => {
+    const accessories = await Accessory.find().lean();
+
+    return accessories
+}
+
+const attachedAccessories = async (cubeId) => {
+    try {
+      const cube = await Cube.findById(cubeId).lean()
+      const accessories = await Accessory.find({ cubes: { $nin: cubeId } }).lean()
+      return { cube, accessories }
+    } catch (err) {
+      return err
+    }
+  }
+  
+  module.exports = {
+    getAccessories,
+    attachedAccessories
+  }
--- controllers/cubes.js
+++ controllers/cubes.js
@@ -11,8 +11,24 @@ const getCube = async (id) => {
     return cube;
 }
 
+const getCubeWithAccessories = async (id) => {
+    const cube = await Cube.findById(id).populate('accessories').lean();
+
+    return cube;
+}
+
+const updateCube = async (cubeId, accessoryId) =>{
+    await Cube.findByIdAndUpdate(cubeId, 
+        { 
+            $addToSet: {
+            accessories: [accessoryId]
+        }
+    } )
+}
 
 module.exports = {
     getAllCubes,
-    getCube
+    getCube,
+    getCubeWithAccessories,
+    updateCube
 }
