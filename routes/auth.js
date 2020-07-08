7dcb7f3bb0572a98b1569b3c7c708f34d618a72f
Nedka Taskova <Ntaskova@intracol.com>
2020-07-08 11:26:01 +0300

    finish signup

--- routes/auth.js
+++ routes/auth.js
@@ -1,14 +1,25 @@
 const { Router } = require('express')
+const { saveUser } = require('../controllers/user')
 
 const router = Router();
 
 router.get('/login', (req, res) => {
-    console.log('login')
     res.render('loginPage')
 })
 
 router.get('/signup', (req, res) => {
     res.render('registerPage')
+})
+
+router.post('/signup',  async (req, res) => {
+
+    const status = await saveUser(req, res)
+    
+    if(status){
+        return res.redirect('/')
+    }
+    
+    res.redirect('/')
 })
 
 router.get('/logout', (req, res) => {
